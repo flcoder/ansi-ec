22ed6e4046c201912c310a182de803f613b47984
William Oney <flcoder@gmail.com>
2019-10-31 15:58:46 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+export const RESET = "\x1b[0m"
+export const REVERSE = "\x1b[7m"
+export const BLACK = "\x1b[30m"
+export const RED = "\x1b[31m"
+export const GREEN = "\x1b[32m"
+export const YELLOW = "\x1b[33m"
+export const CYAN = "\x1b[36m"
+export const REV_RED = REVERSE + RED
+export const REV_GREEN = REVERSE + GREEN
+export const REV_YELLOW = REVERSE + YELLOW
+/*
+FgYellow = "\x1b[33m"
+FgBlue = "\x1b[34m"
+FgMagenta = "\x1b[35m"
+FgCyan = "\x1b[36m"
+FgWhite = "\x1b[37m"
+*/
